75b75b2006739e3c12ad7640e21ca248abce0c9b
jarbasai <jarbasai@mailfence.com>
2021-11-03 22:17:45 +0000

    :tada: - packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+# skill_id=package_name:SkillClass
+PLUGIN_ENTRY_POINT = 'skill-balena-wifi-setup.openvoiceos=ovos_plugin_balena_wifi_setup:WifiConnectSkill'
+
+setup(
+    # this is the package name that goes on pip
+    name='ovos-skill-balena-wifi-setup',
+    version='0.0.1',
+    description='OVOS balena-wifi-setup skill plugin',
+    url='https://github.com/OpenVoiceOS/skill-balena-wifi-setup',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    package_dir={"ovos_plugin_balena_wifi_setup": ""},
+    package_data={'ovos_plugin_balena_wifi_setup': ['locale/*', "ui/*"]},
+    packages=['ovos_plugin_balena_wifi_setup'],
+    include_package_data=True,
+    install_requires=["ovos-plugin-manager>=0.0.2"],
+    keywords='ovos skill plugin',
+    entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
+)
