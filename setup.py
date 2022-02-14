b11e41a112cf2457349766da34f6095c3b861fec
jarbasai <jarbasai@mailfence.com>
2022-02-14 23:02:06 +0000

    dont require OPM in skills

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     package_data={'ovos_skill_balena_wifi_setup': ['locale/*', "ui/*"]},
     packages=['ovos_skill_balena_wifi_setup'],
     include_package_data=True,
-    install_requires=["ovos-plugin-manager>=0.0.2"],
     keywords='ovos skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
 )
